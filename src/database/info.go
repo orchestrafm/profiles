ea46b95df95059844d0d98094291bf4a652817cd
Starz0r <starz0r@starz0r.com>
2019-06-04 20:32:36 -0500

    database: return a profile from an id constraint

---
+++ src/database/info.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"strconv"
+
+	"github.com/spidernest-go/logger"
+)
+
+func SelectProfileById(id uint64) (error, *Profile) {
+	pf := *new(Profile)
+	err := db.SelectFrom("profiles").
+		Where("id = " + strconv.FormatUint(id, 10)).
+		Limit(1).
+		One(&pf)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("SQL Execution had an issue when executing.")
+		return err, nil
+	}
+
+	pf.UUID = *new(string) // Clear this field so it doesn't return
+
+	return nil, &pf
+}
