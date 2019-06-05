781b44d5286fbc4db9dce5fae84572178993ad60
Starz0r <starz0r@starz0r.com>
2019-06-04 21:57:17 -0500

    routers: find a profile by a id parameter

--- src/routers/get.go
+++ src/routers/get.go
@@ -2,10 +2,33 @@ package routers
 
 import (
 	"net/http"
+	"strconv"
 
-	"github.com/labstack/echo"
+	"github.com/orchestrafm/profiles/src/database"
+	"github.com/spidernest-go/logger"
+	"github.com/spidernest-go/mux"
 )
 
-func getProfile(c echo.Context) {
-	c.String(http.StatusOK, "Hello, world!")
+func getProfileById(c echo.Context) error {
+	// Check for valid real numbers
+	i, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msgf("Passed id parameter (%s) was not a valid number", c.Param("id"))
+
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+
+	// Get profile information
+	err, pf := database.SelectProfileById(i)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Profile specified either does not exist or requesting user is unauthorized.")
+
+		c.JSON(http.StatusNotFound, ErrGeneric)
+	}
+
+	return c.JSON(http.StatusOK, &pf)
 }
--- src/routers/serve.go
+++ src/routers/serve.go
@@ -1,16 +1,18 @@
 package routers
 
 import (
-	"github.com/labstack/echo"
+	"github.com/spidernest-go/mux"
 )
 
 var r *echo.Echo
+
+const ErrGeneric = `{"errno": "404", "message": "Bad Request"}`
 
 func ListenAndServe() {
 	r = echo.New()
 
 	v0 := r.Group("/api/v0")
-	v0.GET("/profile/:id")
+	v0.GET("/profile/:id", getProfileById)
 
 	r.Start(":5000")
 }
