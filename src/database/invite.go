d86a19663abd0d829c2c6585680a679fcc68a1de
Starz0r <starz0r@starz0r.com>
2019-11-24 15:11:18 -0600

    database: unburn invite codes if needed

--- src/database/invite.go
+++ src/database/invite.go
@@ -42,3 +42,32 @@ func BurnInvite(code string) error {
 	}
 	return err
 }
+
+func UnburnInvite(code string) error {
+	invites := db.Collection("invites")
+	rs := invites.Find("code", code)
+	i := *new(Invite)
+	err := rs.One(&i)
+	if err != nil && err != sql.ErrNoRows {
+		logger.Error().
+			Err(err).
+			Msg("Bad parameters or database error.")
+
+		return err
+	}
+
+	if i.Burned == false {
+		logger.Warn().
+			Msg("Invite code has yet to be used.")
+		return nil
+	}
+
+	i.Burned = false
+	err = rs.Update(i)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Invite Code could not be updated from the table.")
+	}
+	return err
+}
