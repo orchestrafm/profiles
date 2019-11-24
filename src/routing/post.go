8deae89ef65fdee2e3af6f688ead2f82c25c593e
Starz0r <starz0r@starz0r.com>
2019-11-24 12:37:43 -0600

    routers: allow profiles to be created with invite codes

---
+++ src/routing/post.go
@@ -0,0 +1,54 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/orchestrafm/profiles/src/database"
+	"github.com/orchestrafm/profiles/src/identity"
+	"github.com/spidernest-go/logger"
+	"github.com/spidernest-go/mux"
+)
+
+func createProfile(c echo.Context) error {
+	// Validate Data
+	reg := new(database.Registration)
+	if err := c.Bind(reg); err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Invalid or malformed registration form.")
+
+		return c.JSON(http.StatusNotAcceptable, &struct {
+			Message string
+		}{
+			Message: "Registration form data was invalid or malformed."})
+	}
+
+	// Burn Invite Code and reject if already burned
+	err := database.BurnInvite(reg.InviteCode)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, &struct {
+			Message string
+		}{
+			Message: "Invite Code is invalid or already used."})
+	}
+
+	// Setup Profile
+	uuid, err := identity.NewAccount(reg.Username, reg.Email, reg.Password)
+	if err != nil { //TODO: Unburn invite code
+		return c.JSON(http.StatusInternalServerError, &struct {
+			Message string
+		}{
+			Message: "Identity Server failed to create the account."})
+	}
+	p := new(database.Profile)
+	p.UUID = uuid
+	err = p.New()
+	if err != nil { //TODO: Unburn invite code too
+		return c.JSON(http.StatusInternalServerError, &struct {
+			Message string
+		}{
+			Message: "Database could not be reached."})
+	}
+
+	return c.JSON(http.StatusOK, p)
+}
--- src/routing/serve.go
+++ src/routing/serve.go
@@ -13,6 +13,7 @@ func ListenAndServe() {
 
 	v0 := r.Group("/api/v0")
 	v0.GET("/profile/:id", getProfileById)
+	v0.POST("/profile", createProfile)
 
 	r.Start(":5000")
 }
