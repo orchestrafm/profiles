192d21457f4ea855d85900bf5869f5795b6f1d18
Starz0r <starz0r@starz0r.com>
2019-11-24 12:35:28 -0600

    database: burn invite codes on use

---
+++ src/database/invite.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/spidernest-go/logger"
+)
+
+type Invite struct {
+	ID     uint64 `db:"id"`
+	Code   string `db:"code"`
+	Burned bool   `db:"burned"`
+}
+
+func BurnInvite(code string) error {
+	invites := db.Collection("invites")
+	//rs := invites.Find(code) // BUG: This doesn't work, not sure why
+	rs := invites.Find("code", code)
+	i := *new(Invite)
+	err := rs.One(&i)
+	if err != nil && err != sql.ErrNoRows {
+		logger.Error().
+			Err(err).
+			Msg("Bad parameters or database error.")
+
+		return err
+	}
+
+	if i.Burned == true {
+		logger.Warn().
+			Msg("Invite Code was already burned.")
+
+		return errors.New("Invite code was already burned.")
+	}
+	i.Burned = true
+	err = rs.Update(i)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Invite Code could not be updated from the table.")
+	}
+	return err
+}
