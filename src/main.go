685d04ab885b11e4e4c8214074402cbb2f455e91
Starz0r <starz0r@starz0r.com>
2019-11-24 12:41:56 -0600

    main: constantly maintain connection with idp

--- src/main.go
+++ src/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"time"
+
 	"github.com/orchestrafm/profiles/src/database"
+	"github.com/orchestrafm/profiles/src/identity"
 	"github.com/orchestrafm/profiles/src/routing"
 	"github.com/spidernest-go/logger"
 )
 
 func main() {
 	err := database.Connect()
-	logger.Error().
-		Err(err).
-		Msg("MySQL Database could not be attached to.")
 	if err != nil {
 		logger.Fatal().
 			Err(err).
 			Msg("MySQL Database could not be attached to.")
 	}
 	database.Synchronize()
+
+	identity.Handshake()
+	job := time.NewTicker(time.Minute)
+	go func() {
+		for {
+			<-job.C
+			identity.Handshake()
+		}
+	}()
 
 	routers.ListenAndServe()
 }
