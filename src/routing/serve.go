d74c0091dccb14408a49ce57eeb31bc5fa23c8ec
Starz0r <starz0r@starz0r.com>
2019-12-07 00:30:49 -0600

    routers: initialize cryptography for oidc api routes

--- src/routing/serve.go
+++ src/routing/serve.go
@@ -1,14 +1,36 @@
 package routers
 
 import (
+	"sync"
+
+	"github.com/emirpasic/gods/lists/arraylist"
+	"github.com/orchestrafm/profiles/src/identity"
+	"github.com/spidernest-go/logger"
 	"github.com/spidernest-go/mux"
 )
 
-var r *echo.Echo
+var (
+	r *echo.Echo
+
+	OAuthStates *arraylist.List
+	StateLock   *sync.RWMutex
+)
 
 const ErrGeneric = `{"errno": "404", "message": "Bad Request"}`
 
 func ListenAndServe() {
+	// Initalize stuff for OAuth2 and OpenID Connect
+	err := identity.InitRandomPool()
+	if err != nil {
+		logger.Fatal().
+			Err(err).
+			Msg("Randomness pool could not be filled, entropy on the current system might be low.")
+	}
+
+	StateLock = &sync.RWMutex{}
+	OAuthStates = arraylist.New()
+
+	// Start serving API routes
 	r = echo.New()
 
 	v0 := r.Group("/api/v0")
