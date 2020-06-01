c13d226228bfa4493657ded143eccc142d094ff9
Starz0r <starz0r@starz0r.com>
2020-06-01 13:20:57 -0500

    identity: shim over refreshing tokens

--- src/identity/accounts.go
+++ src/identity/accounts.go
@@ -34,3 +34,11 @@ func GetAccount(uuid string) (*gocloak.User, error) {
 		os.Getenv("IDP_REALM"),
 		uuid)
 }
+
+func RefreshToken(ref string) (*gocloak.JWT, error) {
+	return idp.RefreshToken(
+		ref,
+		os.Getenv("OIDC_CLIENT_ID"),
+		os.Getenv("OIDC_CLIENT_SECRET"),
+		os.Getenv("IDP_REALM"))
+}
