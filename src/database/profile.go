afc4f8f159c4ab2b6a38fd4c28fda033a2fe69e6
Starz0r <starz0r@starz0r.com>
2020-08-10 04:59:16 -0500

    database: groups field on profile type struct

--- src/database/profile.go
+++ src/database/profile.go
@@ -1,15 +1,16 @@
 package database
 
 type Profile struct {
-	ID                uint64 `db:"id" json:"id"`
-	UUID              string `db:"uuid" json:"uuid,omitempty"`
-	Username          string `json:"name,omitempty"`
-	Experience        uint64 `db:"experience" json:"experience"`
-	Level             uint64 `db:"level" json:"level"`
-	TotalScore        uint64 `db:"total_score" json:"total_score"`
-	PlayCount         uint64 `db:"play_count" json:"play_count"`
-	Mastery           uint8  `db:"mastery" json:"mastery"`
-	PerformanceRating uint64 `db:"performance_rating" json:"performance_rating"`
+	ID                uint64   `db:"id" json:"id"`
+	UUID              string   `db:"uuid" json:"uuid,omitempty"`
+	Username          string   `json:"name,omitempty"`
+	Groups            []string `json:"groups,omitempty"`
+	Experience        uint64   `db:"experience" json:"experience"`
+	Level             uint64   `db:"level" json:"level"`
+	TotalScore        uint64   `db:"total_score" json:"total_score"`
+	PlayCount         uint64   `db:"play_count" json:"play_count"`
+	Mastery           uint8    `db:"mastery" json:"mastery"`
+	PerformanceRating uint64   `db:"performance_rating" json:"performance_rating"`
 }
 
 type Registration struct {
